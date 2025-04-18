a74537d0d42858e2d535a76058667fe7e2fd124e
Aaleen <aaleenmirza110@gmail.com>
2025-04-18 20:53:33 +0530

    [ush

--- src/screens/auth/login.js
+++ src/screens/auth/login.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useAuth } from "../../context/AuthContext.jsx";
+import { useAuth } from "../../context/AuthContext";
 import axios from "axios";
 import { jwtDecode } from "jwt-decode"; // Add this package to decode JWT tokens
 import toast from "react-hot-toast";
